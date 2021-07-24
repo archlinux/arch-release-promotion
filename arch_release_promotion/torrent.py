f271be6d25ff285db9aa39d4cc2da50ec233fca5
David Runge <dvzrv@archlinux.org>
2021-07-25 00:17:30 +0200

    Add integration for torrent creation
    arch_release_promotion/torrent.py:
    Add torrent file creation with webseeds using torrentool.
    
    tests/test_torrent.py:
    Add tests for torrent file creation.

---
+++ arch_release_promotion/torrent.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+from typing import List
+from urllib.request import urlopen
+
+from torrentool.api import Torrent
+
+
+def create_torrent_file(path: Path, webseeds: List[str], output: Path) -> None:
+    """Create a torrent file for a path and write it to an output directory
+
+    Parameters
+    ----------
+    path: Path
+        The path for which to create a torrent file
+    webseeds: List[str]
+        The list of webseeds to add to the torrent file
+    output: Path
+        A path to write the .torrent file to
+    """
+
+    torrent = Torrent.create_from(path)
+    torrent.webseeds = webseeds
+    torrent.to_file(output)
+
+
+def get_webseeds(
+    artifact_type: str,
+    mirrorlist_url: str,
+    version: str,
+) -> List[str]:
+    """Read available mirrors from a remote URL and return them in a formatted list representing the webseeds for a
+    given artifact type
+
+    Parameters
+    ----------
+    artifact_type: str
+        The artifact type to create the webseeds for
+    mirrorlist_url: str
+        A URL used to retrieve the list of mirrors
+    version: str
+        The version of the artifact type to create the webseeds for
+
+    Returns
+    -------
+    List[str]
+        A list of strings representing webseeds for an artifact type in a specific version
+    """
+
+    webseeds: List[str] = []
+    url = urlopen(mirrorlist_url)
+    for line in url.read().decode("utf-8").split("\n"):
+        if "#Server = " in line:
+            webseeds += [
+                line.replace("#Server = ", "",).replace(
+                    "$repo/os/$arch",
+                    f"releases/{artifact_type}/{version}/",
+                )
+            ]
+
+    return webseeds
