dac92cdb421dc723be2cfcc88b35d6fa7d8bf748
David Runge <dvzrv@archlinux.org>
2021-07-25 00:36:47 +0200

    Add configuration layer
    arch_release_promotion/config.py:
    Add pydantic based configuration layer that allows to read
    packager/signer information from an existing makepkg configuration and
    project specific information from TOML files in XDG config home
    compatible locations.
    
    tests/test_config.py:
    Add tests for the various configuration classes and models.

---
+++ tests/test_config.py
@@ -0,0 +1,125 @@
+import random
+import tempfile
+from contextlib import nullcontext as does_not_raise
+from pathlib import Path
+from string import ascii_uppercase
+from typing import ContextManager, List
+from unittest.mock import patch
+
+from pydantic import ValidationError
+from pytest import mark, raises
+
+from arch_release_promotion import config
+
+
+@mark.parametrize(
+    "gpgkey, packager, expectation",
+    [
+        (
+            "".join(random.choice(ascii_uppercase) for x in range(40)),
+            "Foobar McFoo <foobar@archlinux.org>",
+            does_not_raise(),
+        ),
+        (
+            "".join(random.choice(ascii_uppercase) for x in range(40)),
+            "",
+            raises(ValueError),
+        ),
+        (
+            "".join(random.choice(ascii_uppercase) for x in range(40)),
+            "Foobar McFoo",
+            raises(ValueError),
+        ),
+        (
+            "".join(random.choice(ascii_uppercase) for x in range(40)),
+            "<foobar@archlinux.org>",
+            raises(ValueError),
+        ),
+        (
+            "".join(random.choice(ascii_uppercase) for x in range(40)),
+            "Foobar McFoo <foobar@mc.fooface>",
+            raises(ValueError),
+        ),
+        (
+            "".join(random.choice(ascii_uppercase) for x in range(10)),
+            "Foobar McFoo <foobar@archlinux.org>",
+            raises(ValueError),
+        ),
+    ],
+)
+def test_settings(
+    gpgkey: str,
+    packager: str,
+    expectation: ContextManager[str],
+) -> None:
+    conf = tempfile.NamedTemporaryFile(mode="w", encoding="utf-8", suffix=".conf", delete=False)
+    conf.write(f"GPGKEY='{gpgkey}'\n")
+    conf.write(f"PACKAGER='{packager}'\n")
+    conf.close()
+
+    with patch("arch_release_promotion.config.MAKEPKG_CONFIGS", [Path(conf.name)]):
+        with expectation:
+            assert config.Settings()
+    Path(conf.name).unlink()
+
+
+@mark.parametrize(
+    "create_config, config_rows, name, expectation",
+    [
+        (
+            True,
+            [
+                "[[projects]]",
+                'name = "foo/bar"',
+                'job_name = "build"',
+                'metrics_file = "metrics.txt"',
+                'output_dir = "output"',
+                'releases = [{name = "test",info_metrics = ["bar"],extensions_to_sign = [".baz"]}]',
+            ],
+            "foo/bar",
+            does_not_raise(),
+        ),
+        (
+            True,
+            [
+                "[[projects]]",
+                'name = "foo/bar"',
+                'job_name = "build"',
+                'metrics_file = "metrics.txt"',
+                'output_dir = "output"',
+                'releases = [{name = "test",info_metrics = ["bar"],extensions_to_sign = [".baz"]}]',
+            ],
+            "foo/baz",
+            raises(RuntimeError),
+        ),
+        (
+            False,
+            [],
+            "",
+            raises(ValidationError),
+        ),
+    ],
+)
+def test_projects(
+    create_config: bool,
+    config_rows: List[str],
+    name: str,
+    expectation: ContextManager[str],
+) -> None:
+    if create_config:
+        conf = tempfile.NamedTemporaryFile(mode="w", encoding="utf-8", suffix=".conf", delete=False)
+        for row in config_rows:
+            conf.write(f"{row}\n")
+        conf.close()
+
+        with patch("arch_release_promotion.config.PROJECTS_CONFIGS", [Path(conf.name)]):
+            with expectation:
+                projects = config.Projects()
+                assert projects
+                assert isinstance(projects.get_project(name=name), config.ProjectConfig)
+
+        Path(conf.name).unlink()
+    else:
+        with patch("arch_release_promotion.config.PROJECTS_CONFIGS", [Path("foo.bar")]):
+            with expectation:
+                assert config.Projects()
