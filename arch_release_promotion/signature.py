1ffab40f3b0b0e065454a4f00a61b41c7ecf7752
David Runge <dvzrv@archlinux.org>
2021-07-25 22:23:29 +0200

    Iteratively attempt to sign files
    arch_release_promotion/signature.py:
    Add `sign_file()` to sign a single file and return the status code of
    the gpg call.
    Change `sign_files_in_dir()` to call `sign_file()` iteratively per file
    until a status code of 0 is emitted.
    
    tests/test_signature.py:
    Add tests for `sign_file()` and adapt tests for the changes to
    `sign_files_in_dir()`.

--- arch_release_promotion/signature.py
+++ arch_release_promotion/signature.py
@@ -19,15 +19,38 @@ def sign_files_in_dir(path: Path, developer: str, gpgkey: str, file_extensions: 
     for _file in path.iterdir():
         if _file.suffix in file_extensions:
             print(f"Creating signature for {_file}...")
-            run(
-                [
-                    "gpg",
-                    "--sender",
-                    developer,
-                    "--default-key",
-                    gpgkey,
-                    "--detach-sign",
-                    str(_file.resolve()),
-                ],
-            )
+            return_code = 255
+            while return_code != 0:
+                return_code = sign_file(path=_file.resolve(), developer=developer, gpgkey=gpgkey)
             print("Done!")
+
+
+def sign_file(path: Path, developer: str, gpgkey: str) -> int:
+    """Sign a file and return the status code of gpg
+
+    Parameters
+    ----------
+    path: Path
+        The path to a file to sign
+    developer: str
+        The developer mbox (i.e. "First Last <user@domain.tld>")
+    gpgkey: str
+        The PGP key to sign with
+
+    Returns
+    -------
+    int
+        The returncode of the gpg process
+    """
+
+    return run(
+        [
+            "gpg",
+            "--sender",
+            developer,
+            "--default-key",
+            gpgkey,
+            "--detach-sign",
+            str(path),
+        ],
+    ).returncode
